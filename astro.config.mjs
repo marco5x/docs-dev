3c1d8176c6c0c8df3e24453195d43d3154efbd91
MarcosX <79601596+Marco5X@users.noreply.github.com>
2023-07-11 18:57:18 -0300

    Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
@@ -6,4 +7,5 @@ export default defineConfig({
   site: "https://docstools.dev",
   integrations: [robotsTxt()],
   output: "server",
+  adapter: cloudflare({ mode: "directory" }),
 });
